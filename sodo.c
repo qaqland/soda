5ecc070ba83f6e0e84d75f462e6329b3df7c6ad0
qaqland <qaq@qaq.land>
2025-09-04 00:20:44 +0800

    add sodoedit (1/3)

--- sodo.c
+++ sodo.c
@@ -1,5 +1,6 @@
 #define _GNU_SOURCE
 #include <assert.h>
+#include <fcntl.h>
 #include <grp.h>
 #include <libgen.h>
 #include <limits.h>
@@ -8,9 +9,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/sendfile.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #define WHEEL_GROUP "wheel"
+#define LAST_EDITOR "vi"
 
 #ifdef NDEBUG
 #define LOG(...) ((void) 0)
@@ -23,20 +28,31 @@
 #else
 #define LOG(fmt, ...)                                                          \
 	do {                                                                   \
-		fprintf(stderr, "[DEBUG] %d | ", __LINE__);                    \
+		fprintf(stderr, "[DEBUG] %3d | ", __LINE__);                   \
 		fprintf(stderr, fmt, ##__VA_ARGS__);                           \
 		fputc('\n', stderr);                                           \
 	} while (0)
 #define ERR(fmt, ...)                                                          \
 	do {                                                                   \
-		fprintf(stderr, "[ERROR] %d | ", __LINE__);                    \
+		fprintf(stderr, "[ERROR] %3d | ", __LINE__);                   \
 		fprintf(stderr, fmt, ##__VA_ARGS__);                           \
 		fputc('\n', stderr);                                           \
 		exit(EXIT_FAILURE);                                            \
 	} while (0)
 #endif
 
-bool in_wheel(uid_t ruid) {
+struct edit_file {
+	const char *old_path;
+	char *tmp_path;
+	int old_fd;
+	int tmp_fd;
+	struct stat stat;
+};
+
+uid_t ruid, euid, suid;
+gid_t rgid, egid, sgid;
+
+bool check_user_group(void) {
 	struct passwd *pw = getpwuid(ruid);
 	struct group *gr = getgrnam(WHEEL_GROUP);
 	for (int i = 0; gr->gr_mem[i]; i++) {
@@ -48,45 +64,51 @@ bool in_wheel(uid_t ruid) {
 	return false;
 }
 
-const char *get_editor(void) {
-	const char *editors[] = {
-		"SUDO_EDITOR", // sudo
-		"DOAS_EDITOR", // doas
-		"EDITOR",      "VISUAL", NULL,
-	};
-	for (int i = 0; editors[i]; i++) {
-		const char *value = getenv(editors[i]);
-		if (value && *value) {
-			return value;
-		}
-	}
-	return "vi";
-}
-
-const char *find_cmd_path(const char *cmd) {
-	if (strchr(cmd, '/')) {
-		return cmd;
+const char *find_exec_path(const char *exec) {
+	if (strchr(exec, '/')) {
+		return exec;
 	}
 
-	char *path_env = strdup(getenv("PATH"));
-	char *cmd_path = NULL;
-	char *token = strtok(path_env, ":");
-	char fullpath[PATH_MAX] = {0};
+	char *full_path = strdup(getenv("PATH"));
+	char *exec_path = NULL;
+
+	char buff_path[PATH_MAX] = {0};
+	char *token = strtok(full_path, ":");
 	while (token) {
-		snprintf(fullpath, PATH_MAX, "%s/%s", token, cmd);
-		if (access(fullpath, F_OK) == 0) {
-			cmd_path = fullpath;
+		snprintf(buff_path, PATH_MAX, "%s/%s", token, exec);
+		if (access(buff_path, F_OK) == 0) {
+			exec_path = buff_path;
 			break;
 		}
 		token = strtok(NULL, ":");
 	}
-	free(path_env);
-	LOG("find cmd_path: %s -> %s", cmd, cmd_path);
-	return cmd_path ? strdup(cmd_path) : cmd;
+	free(full_path);
+
+	LOG("find_exec_path: %s -> %s", exec, exec_path ? exec_path : "NULL");
+	return exec_path ? strdup(exec_path) : NULL;
 }
 
-char **backupenv(void) {
-	extern char **environ;
+const char *find_editor(void) {
+	const char *items[] = {
+		"EDITOR",
+		"VISUAL",
+		// others
+		NULL,
+	};
+	const char *name = LAST_EDITOR;
+	for (int i = 0; items[i]; i++) {
+		const char *value = getenv(items[i]);
+		if (value && *value) {
+			name = value;
+			break;
+		}
+	}
+
+	LOG("find_editor: %s", name);
+	return find_exec_path(name);
+}
+
+char **save_user_envp(void) {
 	int env_count = 0;
 	while (environ[env_count]) {
 		env_count++;
@@ -97,10 +119,19 @@ char **backupenv(void) {
 		env_backup[i] = strdup(environ[i]);
 	}
 	assert(env_backup[env_count] == NULL);
+
+	LOG("save_user_envp: *%d", env_count);
 	return env_backup;
 }
 
-void setrootenv(void) {
+void free_user_envp(char **envp) {
+	for (int i = 0; envp[i]; i++) {
+		free(envp[i]);
+	}
+	free(envp);
+}
+
+void init_root_envp(void) {
 	const char *term = getenv("TERM");
 
 	clearenv();
@@ -111,8 +142,9 @@ void setrootenv(void) {
 	setenv("SHELL", "/bin/sh", true);
 	// safe path
 	setenv("PATH",
-	       "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/"
-	       "sbin:/bin",
+	       "/usr/local/sbin:/usr/local/bin:"
+	       "/usr/sbin:/usr/bin:"
+	       "/sbin:/bin",
 	       true);
 	// others
 	setenv("LANG", "C.UTF-8", true);
@@ -140,6 +172,131 @@ int setoptenv(int argc, char *argv[]) {
 	return count;
 }
 
+struct edit_file *copy_tmp_file(const char *path, const char *prefix) {
+	if (!path) {
+		return NULL;
+	}
+	struct edit_file *file = calloc(1, sizeof(*file));
+	file->old_path = path;
+
+	if (stat(path, &file->stat)) {
+		perror("stat");
+		goto stat_err;
+	}
+	if (!S_ISREG(file->stat.st_mode)) {
+		goto stat_err;
+	}
+
+	file->old_fd = open(path, O_RDONLY);
+	if (file->old_fd == -1) {
+		goto open_err;
+	}
+
+	char *fullpath = strdup(path);
+	const char *base = basename(fullpath);
+	char tmpname[PATH_MAX] = {0};
+	snprintf(tmpname, PATH_MAX, "%s/sodo.XXXXXX%s", prefix, base);
+	free(fullpath); // after base
+
+	file->tmp_fd = mkstemps(tmpname, strlen(base));
+	if (file->tmp_fd == -1) {
+		perror("mkstemps");
+		goto temp_err;
+	}
+	file->tmp_path = strdup(tmpname);
+
+	ssize_t sent =
+		sendfile(file->tmp_fd, file->old_fd, NULL, file->stat.st_size);
+	if (sent == -1) {
+		perror("sendfile");
+		goto send_err;
+	}
+
+	lseek(file->old_fd, 0, SEEK_SET);
+	lseek(file->tmp_fd, 0, SEEK_SET);
+
+	fchown(file->tmp_fd, ruid, rgid);
+
+	struct timespec times[2];
+	// access time
+	times[0].tv_sec = file->stat.st_atime;
+	times[0].tv_nsec = 0;
+	// modify time
+	times[1].tv_sec = file->stat.st_mtime;
+	times[1].tv_nsec = 0;
+	futimens(file->tmp_fd, times);
+
+	return file;
+
+send_err:
+	close(file->tmp_fd);
+	unlink(file->tmp_path);
+	free(file->tmp_path);
+temp_err:
+	close(file->old_fd);
+open_err:
+stat_err:
+	free(file);
+	return NULL;
+}
+
+struct edit_file **init_edit_files(int argc, char *argv[]) {
+	const char *prefix = "/tmp";
+
+	struct edit_file **files = calloc(argc - optind + 1, sizeof(*files));
+
+	int count = 0;
+	for (int i = optind; i < argc; i++) {
+		struct edit_file *file = copy_tmp_file(argv[i], prefix);
+		if (!file) {
+			continue;
+		}
+		LOG("edit_file [%d] %s -> %s", count + 1, file->old_path,
+		    file->tmp_path);
+		files[count++] = file;
+	}
+
+	assert(files[count] == NULL);
+	if (count) {
+		return files;
+	} else {
+		free(files);
+		return NULL;
+	}
+}
+
+void move_edit_files(struct edit_file **files) {
+	for (int i = 0; files[i]; i++) {
+		struct stat new_stat;
+		fstat(files[i]->tmp_fd, &new_stat);
+		if (new_stat.st_mtime == files[i]->stat.st_mtime) {
+			LOG("move_edit_files: skip %s", files[i]->old_path);
+			continue;
+		}
+		rename(files[i]->tmp_path, files[i]->old_path);
+		// failed: copy
+		// chmod?
+		// remove tmp_path
+	}
+}
+
+char **make_edit_argv(struct edit_file **files) {
+	int count = 0;
+	while (files[count]) {
+		count++;
+	}
+	assert(count);
+
+	char **argv = calloc(count + 2, sizeof(*argv));
+	// argv[0] = editor
+	for (int i = 0; i < count; i++) {
+		argv[i + 1] = files[i]->tmp_path;
+	}
+
+	assert(argv[count + 1] == NULL);
+	return argv;
+}
+
 int main(int argc, char *argv[]) {
 	const char *editor = NULL;
 
@@ -150,7 +307,7 @@ int main(int argc, char *argv[]) {
 	while ((opt = getopt(argc, argv, "ehv")) != -1) {
 		switch (opt) {
 		case 'e':
-			editor = get_editor();
+			editor = find_editor();
 			break;
 		case 'h':
 		case 'v':
@@ -167,11 +324,15 @@ int main(int argc, char *argv[]) {
 		ERR("args?");
 	}
 
-	const char *cmd_path = find_cmd_path(argv[optind]);
+	const char *exec_path = find_exec_path(argv[optind]);
 
-	uid_t ruid, euid, suid;
 	if (getresuid(&ruid, &euid, &suid) == -1) {
 		perror("getresuid");
+		exit(EXIT_FAILURE);
+	}
+
+	if (getresgid(&rgid, &egid, &sgid) == -1) {
+		perror("getresgid");
 		exit(EXIT_FAILURE);
 	}
 
@@ -186,11 +347,11 @@ int main(int argc, char *argv[]) {
 	}
 
 	// wheel?
-	if (!in_wheel(ruid)) {
+	if (!check_user_group()) {
 		ERR("wheel?");
 	}
 
-	char *const *old_env = backupenv();
+	char *const *old_env = save_user_envp();
 
 	if (setuid(0) == -1) {
 		perror("setuid");
@@ -201,15 +362,41 @@ int main(int argc, char *argv[]) {
 		exit(EXIT_FAILURE);
 	}
 
-	if (editor) {
-		// TODO
-	}
-
-	setrootenv();
+	init_root_envp();
 	setoptenv(argc, argv);
 
-	// gogogo
-	execvp(cmd_path, &argv[optind]);
-	perror("execvp");
-	exit(EXIT_FAILURE);
+	if (!editor) {
+		// gogogo
+		execvp(exec_path, &argv[optind]);
+		perror("execvp");
+		exit(EXIT_FAILURE);
+	}
+
+	// mktemp, cp, chown, time
+	struct edit_file **files = init_edit_files(argc, argv);
+	if (!files) {
+		ERR("file count = 0");
+	}
+	char **edit_argv = make_edit_argv(files);
+	edit_argv[0] = (char *) editor;
+
+	// fork
+	pid_t pid = fork();
+	int wstatus;
+	switch (pid) {
+	case -1:
+		perror("fork");
+		exit(EXIT_FAILURE);
+	case 0:
+		setgid(rgid); // 1 st
+		setuid(ruid); // 2 nd
+		execvpe(editor, edit_argv, old_env);
+		perror(editor);
+	// vim
+	default:
+		// wait
+		waitpid(pid, &wstatus, 0);
+		move_edit_files(files);
+		exit(EXIT_SUCCESS);
+	}
 }
