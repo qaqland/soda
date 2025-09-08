b76323c06e1957fab348ecbeea21216157da394e
qaqland <qaq@qaq.land>
2025-09-09 00:20:48 +0800

    improve log output

--- sodo.c
+++ sodo.c
@@ -21,27 +21,54 @@
 	"/usr/local/sbin:/usr/local/bin:"                                      \
 	"/usr/sbin:/usr/bin:"                                                  \
 	"/sbin:/bin"
+#define PROGRAM_NAME "sodo"
 
 #ifdef NDEBUG
-#define LOG(...) ((void) 0)
+#define LOG(fmt, ...)                                                          \
+	do {                                                                   \
+		fprintf(stderr, PROGRAM_NAME ": " fmt "\n", ##__VA_ARGS__);    \
+	} while (0)
+#define LOG_SYS(fmt, ...)                                                      \
+	do {                                                                   \
+		int _errno = errno;                                            \
+		fprintf(stderr, PROGRAM_NAME ": " fmt " (errno: %d - %s)\n",   \
+			##__VA_ARGS__, _errno, strerror(_errno));              \
+	} while (0)
 #define ERR(fmt, ...)                                                          \
 	do {                                                                   \
-		fprintf(stderr, fmt, ##__VA_ARGS__);                           \
-		fputc('\n', stderr);                                           \
+		fprintf(stderr, PROGRAM_NAME ": " fmt "\n", ##__VA_ARGS__);    \
+		exit(EXIT_FAILURE);                                            \
+	} while (0)
+#define ERR_SYS(fmt, ...)                                                      \
+	do {                                                                   \
+		int _errno = errno;                                            \
+		fprintf(stderr, PROGRAM_NAME ": " fmt " (errno: %d - %s)\n",   \
+			##__VA_ARGS__, _errno, strerror(_errno));              \
 		exit(EXIT_FAILURE);                                            \
 	} while (0)
 #else
 #define LOG(fmt, ...)                                                          \
 	do {                                                                   \
-		fprintf(stderr, "[DEBUG] %3d | ", __LINE__);                   \
-		fprintf(stderr, fmt, ##__VA_ARGS__);                           \
-		fputc('\n', stderr);                                           \
+		fprintf(stderr, "[%3d] DEBUG: " fmt "\n", __LINE__,            \
+			##__VA_ARGS__);                                        \
+	} while (0)
+#define LOG_SYS(fmt, ...)                                                      \
+	do {                                                                   \
+		int _errno = errno;                                            \
+		fprintf(stderr, "[%3d] DEBUG: " fmt " (errno: %d - %s)\n",     \
+			__LINE__, ##__VA_ARGS__, _errno, strerror(_errno));    \
 	} while (0)
 #define ERR(fmt, ...)                                                          \
 	do {                                                                   \
-		fprintf(stderr, "[ERROR] %3d | ", __LINE__);                   \
-		fprintf(stderr, fmt, ##__VA_ARGS__);                           \
-		fputc('\n', stderr);                                           \
+		fprintf(stderr, "[%3d] ERROR: " fmt "\n", __LINE__,            \
+			##__VA_ARGS__);                                        \
+		exit(EXIT_FAILURE);                                            \
+	} while (0)
+#define ERR_SYS(fmt, ...)                                                      \
+	do {                                                                   \
+		int _errno = errno;                                            \
+		fprintf(stderr, "[%3d] ERROR: " fmt " (errno: %d - %s)\n",     \
+			__LINE__, ##__VA_ARGS__, _errno, strerror(_errno));    \
 		exit(EXIT_FAILURE);                                            \
 	} while (0)
 #endif
@@ -57,29 +84,27 @@ struct edit_file {
 uid_t ruid, euid, suid;
 gid_t rgid, egid, sgid;
 
-bool check_user_group(void) {
+void check_user_group(void) {
 	struct passwd *pw = getpwuid(ruid);
 	if (!pw) {
-		perror("getpwuid");
-		return false;
+		ERR_SYS("failed to get user info for UID %d", ruid);
 	}
 	struct group *gr = getgrnam(WHEEL_GROUP);
 	if (!gr) {
-		perror("getgrnam");
-		return false;
+		ERR_SYS("group " WHEEL_GROUP " not found");
 	}
 	for (int i = 0; gr->gr_mem[i]; i++) {
 		const char *name = gr->gr_mem[i];
 		if (strcmp(name, pw->pw_name) == 0) {
-			return true;
+			return;
 		}
 	}
-	return false;
+	ERR("failed to verify user in " WHEEL_GROUP " group");
 }
 
-const char *find_exec_path(const char *exec) {
+char *find_exec_path(const char *exec) {
 	if (strchr(exec, '/')) {
-		return exec;
+		return strdup(exec);
 	}
 
 	const char *user_path = getenv("PATH");
@@ -98,8 +123,13 @@ const char *find_exec_path(const char *exec) {
 	}
 	free(full_path);
 
-	LOG("find_exec_path: %s -> %s", exec, exec_path ? exec_path : "NULL");
-	return exec_path ? strdup(exec_path) : NULL;
+	if (exec_path) {
+		LOG("retrieve path for command %s: %s", exec, exec_path);
+		return strdup(exec_path);
+	} else {
+		LOG("failed to retrieve path for command %s", exec);
+		return NULL;
+	}
 }
 
 const char *find_user_editor(void) {
@@ -118,7 +148,7 @@ const char *find_user_editor(void) {
 		}
 	}
 
-	LOG("find_user_editor: %s", name);
+	LOG("using editor: %s", name);
 	return name;
 }
 
@@ -134,7 +164,7 @@ char **save_user_envp(void) {
 	}
 	assert(env_backup[env_count] == NULL);
 
-	LOG("save_user_envp: *%d", env_count);
+	LOG("save user environment: entries *%d", env_count);
 	return env_backup;
 }
 
@@ -164,8 +194,9 @@ void init_root_envp(void) {
 }
 
 int set_opts_env(int argc, char *argv[]) {
-	int count = 0;
+	static bool parsed = false;
 
+	int count = 0;
 	for (int i = 1; i < argc; i++) {
 		char *key = strdup(argv[i]);
 		char *value = strchr(key, '=');
@@ -176,9 +207,13 @@ int set_opts_env(int argc, char *argv[]) {
 		count++;
 		*value = '\0';
 		setenv(key, (value + 1), true);
+		if (!parsed) {
+			LOG("parse argument: %s=%s", key, value);
+		}
 		free(key);
 	}
 
+	parsed = true;
 	return count;
 }
 
@@ -186,44 +221,53 @@ struct edit_file *make_copy(const char *path, const char *prefix) {
 	if (!path) {
 		return NULL;
 	}
+	// FIXME
+	// memory alloction is not checked, including calloc and strdup
 	struct edit_file *file = calloc(1, sizeof(*file));
 	file->old_path = path;
 
-	if (stat(path, &file->stat)) {
-		perror("stat");
+	if (stat(file->old_path, &file->stat)) {
+		LOG_SYS("failed to stat %s", file->old_path);
 		goto stat_err;
 	}
 	if (!S_ISREG(file->stat.st_mode)) {
+		LOG("not a regular file: %s", file->old_path);
 		goto stat_err;
 	}
 
-	file->old_fd = open(path, O_RDWR | O_NOFOLLOW);
+	file->old_fd = open(file->old_path, O_RDWR | O_NOFOLLOW | O_CLOEXEC);
 	if (file->old_fd == -1) {
+		LOG_SYS("failed to open %s", file->old_path);
 		goto open_err;
 	}
 
-	char *fullpath = strdup(path);
-	const char *base = basename(fullpath);
-	char tmpname[PATH_MAX] = {0};
-	snprintf(tmpname, PATH_MAX, "%s/sodo.XXXXXX%s", prefix, base);
-	free(fullpath); // after base
+	char *path_dup = strdup(path);
+	const char *base = basename(path_dup);
+	char buff_path[PATH_MAX] = {0};
+	snprintf(buff_path, PATH_MAX, "%s/sodo.XXXXXX%s", prefix, base);
 
-	file->tmp_fd = mkstemps(tmpname, strlen(base));
+	file->tmp_fd = mkostemps(buff_path, strlen(base), O_CLOEXEC);
+	free(path_dup); // free it after using base
+
 	if (file->tmp_fd == -1) {
-		perror("mkstemps");
+		LOG_SYS("failed to mkstemps %s", buff_path);
 		goto temp_err;
 	}
-	file->tmp_path = strdup(tmpname);
+	file->tmp_path = strdup(buff_path);
 
 	off_t offset = 0;
 	ssize_t sent = sendfile(file->tmp_fd, file->old_fd, &offset,
 				file->stat.st_size);
 	if (sent == -1) {
-		perror("sendfile");
+		LOG_SYS("failed to sendfile from %s to %s", file->old_path,
+			file->tmp_path);
 		goto send_err;
 	}
 
-	fchown(file->tmp_fd, ruid, rgid);
+	if (fchown(file->tmp_fd, ruid, rgid)) {
+		// unreachable
+		ERR_SYS("failed to fchown %s", file->tmp_path);
+	}
 
 	struct timespec times[2];
 	// access time
@@ -232,7 +276,10 @@ struct edit_file *make_copy(const char *path, const char *prefix) {
 	// modify time
 	times[1].tv_sec = file->stat.st_mtime;
 	times[1].tv_nsec = 0;
-	futimens(file->tmp_fd, times);
+	if (futimens(file->tmp_fd, times)) {
+		// unreachable
+		ERR_SYS("failed to futimens %s", file->tmp_path);
+	}
 
 	return file;
 
@@ -257,10 +304,10 @@ struct edit_file **fork_each_file(int argc, char *argv[]) {
 	for (int i = optind; i < argc; i++) {
 		struct edit_file *file = make_copy(argv[i], prefix);
 		if (!file) {
+			LOG("fork file from %s (skip)", file->old_path);
 			continue;
 		}
-		LOG("edit_file [%d] %s -> %s", count + 1, file->old_path,
-		    file->tmp_path);
+		LOG("fork file from %s to %s", file->old_path, file->tmp_path);
 		files[count++] = file;
 	}
 
@@ -274,67 +321,84 @@ struct edit_file **fork_each_file(int argc, char *argv[]) {
 }
 
 void move_back(struct edit_file *file, int *new_fd) {
-	int rv = 0;
-
 	struct stat tmp_stat;
 	struct stat old_stat = file->stat;
 
 	*new_fd = file->tmp_fd;
 
-	fstat(file->tmp_fd, &tmp_stat);
-	if (tmp_stat.st_mtime == old_stat.st_mtime) {
-		LOG("skip %s", file->old_path);
-		goto clear;
+	if (fstat(file->tmp_fd, &tmp_stat)) {
+		// unreachable
+		ERR_SYS("failed to fstat %s", file->tmp_path);
 	}
-	// move
-	rv = rename(file->tmp_path, file->old_path);
-	if (rv == 0) {
-		LOG("rename %s -> %s", file->tmp_path, file->old_path);
+	if (tmp_stat.st_mtime == old_stat.st_mtime) {
+		LOG("unchanged %s", file->old_path);
+		goto clean;
+	}
+
+	// try to move
+	if (rename(file->tmp_path, file->old_path)) {
+		LOG("rename %s to %s", file->tmp_path, file->old_path);
 		return;
+	}
+
+	// TODO
+	// try sendfile?
+	if (errno != EXDEV) {
+		LOG_SYS("failed to rename %s", file->tmp_path);
+		*new_fd = -1;
+		goto clean;
 	}
 
 	// if fails, tmp_fd is useless
 	*new_fd = file->old_fd;
 
-	if (errno != EXDEV) {
-		perror("rename");
-		goto clear;
-	}
-
+	// necessary!
 	lseek(file->old_fd, 0, SEEK_SET);
 	ftruncate(file->old_fd, 0);
+
 	// copy
 	off_t offset = 0;
 	ssize_t sent = sendfile(file->old_fd, file->tmp_fd, &offset,
 				file->stat.st_size);
 	if (sent == -1) {
-		perror("sendfile");
+		LOG_SYS("failed to sendfile from %s to %s", file->tmp_path,
+			file->old_path);
+		goto clean;
 	}
-	LOG("copy %s -> %s", file->tmp_path, file->old_path);
+	LOG("copy %s back to %s", file->tmp_path, file->old_path);
 
-clear:
-	LOG("delete %s", file->tmp_path);
-	unlink(file->tmp_path);
+clean:
+	LOG("delete temp file %s", file->tmp_path);
+	if (unlink(file->tmp_path)) {
+		LOG_SYS("failed to delete %s", file->tmp_path);
+	}
 	return;
 }
 
 void save_each_file(struct edit_file **files) {
-	int rv = 0;
 	for (int i = 0; files[i]; i++) {
 		int new_fd = -1;
 		move_back(files[i], &new_fd);
-
+		if (new_fd == -1) {
+			continue;
+		}
 		struct stat new_stat = files[i]->stat;
-		rv = fchown(new_fd, new_stat.st_uid, new_stat.st_gid);
-		if (rv == -1) {
-			perror("fchown");
+		if (fchown(new_fd, new_stat.st_uid, new_stat.st_gid)) {
+			LOG_SYS("failed to fchown %s", files[i]->old_path);
 		}
-		rv = fchmod(new_fd, new_stat.st_mode);
-		if (rv == -1) {
-			perror("fchmod");
+		if (fchmod(new_fd, new_stat.st_mode)) {
+			LOG_SYS("failed to fchmod %s", files[i]->old_path);
 		}
+	}
+}
+
+void free_each_file(struct edit_file **files) {
+	for (int i = 0; files[i]; i++) {
 		close(files[i]->tmp_fd);
 		close(files[i]->old_fd);
+
+		// fork + execvpe has COW
+		free(files[i]->tmp_path);
 	}
 }
 
@@ -346,7 +410,7 @@ char **make_edit_argv(struct edit_file **files) {
 	assert(count);
 
 	char **argv = calloc(count + 2, sizeof(*argv));
-	// argv[0] = editor
+	// argv[0] is editor
 	for (int i = 0; i < count; i++) {
 		argv[i + 1] = files[i]->tmp_path;
 	}
@@ -357,6 +421,8 @@ char **make_edit_argv(struct edit_file **files) {
 
 int main(int argc, char *argv[]) {
 	bool use_editor = false;
+
+	check_user_group();
 
 	// parse FOO=bar
 	optind += set_opts_env(argc, argv);
@@ -400,11 +466,6 @@ int main(int argc, char *argv[]) {
 	// suid?
 	if (ruid == euid) {
 		ERR("suid?");
-	}
-
-	// wheel?
-	if (!check_user_group()) {
-		ERR("wheel?");
 	}
 
 	char **user_envp = save_user_envp();
@@ -453,20 +514,19 @@ int main(int argc, char *argv[]) {
 		perror("fork");
 		exit(EXIT_FAILURE);
 	case 0:
-		for (int i = 0; files[i]; i++) {
-			close(files[i]->tmp_fd);
-			close(files[i]->old_fd);
-		}
 		setgid(rgid); // 1 st
 		setuid(ruid); // 2 nd
 		execvpe(exec_path, edit_argv, user_envp);
 		perror(exec_path);
 	default:
+		// FIXME
+		// wait execvpe to follow COW?
 		free(edit_argv);
 		free_user_envp(user_envp);
 		// wait
 		waitpid(pid, &wstatus, 0);
 		save_each_file(files);
+		free_each_file(files);
 		exit(EXIT_SUCCESS);
 	}
 }
